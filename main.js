55902e15951410601b838594229ebff487bf1098
Tdonnelly20 <tate.donne@gmail.com>
2023-02-20 14:12:18 -0500

    part 1

--- main.js
+++ main.js
@@ -6,11 +6,12 @@ let moveCar=false;
 let moveCamera=false,moveWithCar=false;
 let reflectionsOn=true;
 let lamp, car, stopSign, street, bunny;
-
-let lightPosition =  scale(1.0,vec4(0.0, 3.0, 0.0));
-let lightAmbient = vec4(0.2, 0.2, 0.3, 1.0);
-let lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
-let lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
+let diffuse=.7;
+let specular=.8;
+let lightPosition =vec4(-2, 5, 1.25,1);
+let lightAmbient = vec4(0.2, 0.2, 0.2, 1);
+let lightDiffuse = vec4(diffuse, diffuse, diffuse, 1.0);
+let lightSpecular = vec4(specular,specular,specular, 1.0);
 
 var materialAmbient = vec4( 1.0, 1.0, 1.0, 1.0 );
 var materialDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
@@ -24,8 +25,7 @@ let cameraPosition= vec3(0,4.5,4.5);
 let cameraTarget = vec3(0, 0, 0.0);
 let cameraUp = vec3(0.0, 1.0, 0.0);
 
-let cameraRotation=0,cameraBounce;
-let cameraBounceUp=true;
+let cameraRotation=0;
 
 let near = .1;
 let far = 100;
@@ -50,12 +50,12 @@ function main() {
 
     // Set clear color
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
+    gl.enable(gl.DEPTH_TEST);
     gl.enable(gl.CULL_FACE);
     gl.cullFace(gl.BACK);
-    gl.enable(gl.DEPTH_TEST);
 
     // Initialize shaders
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     program = initShaders(gl, "vshader", "fshader");
     gl.useProgram(program);
 
@@ -68,16 +68,17 @@ function main() {
     cameraMatrixLoc=gl.getUniformLocation( program, "cameraMatrix" );
     cameraMatrix = lookAt(cameraPosition, cameraTarget , cameraUp);
     gl.uniformMatrix4fv(cameraMatrixLoc, false, flatten(cameraMatrix) );
-    staticLighting();
+    defaultLighting();
     render();
 }
 
 function loadObjectArray(){
     loadObjects();
     objectArray.push(street);
-    //objectArray.push(car);
+    objectArray.push(car);
     objectArray.push(stopSign);
-    //objectArray.push(lamp);
+    objectArray.push(lamp);
+    car.addChild(bunny);
     //objectArray.push(bunny);
     checkObjects();
 }
@@ -96,65 +97,63 @@ function modelViewMatrix(){
 }
 
 function setCameraMatrix(){
-    let bob,pos,eye, target, at;
-    if(!moveCamera) return;
+    let bob,pos,target;
 
-    cameraRotation+=1.0;
+    bob=scale(Math.sin(cameraRotation / 25), vec4(0.0, .2500, 0.0, 0.0));
 
-    bob=scale(Math.sin(cameraRotation / 25), vec4(0.0, 1.00, 0.0, 0.0));
-
-    pos=add(mult(rotateY(cameraRotation),vec4(...cameraPosition)),bob);
+    //pos=add(mult(rotateY(cameraRotation),vec4(...cameraPosition)),bob);
+    pos=mult(rotateY(cameraRotation),vec4(...cameraPosition));
     target=mult(rotateY(cameraRotation),vec4(...cameraTarget));
-    eye=vec3(pos[0],pos[1],pos[2]);
-    at=vec3([target[0],target[1],target[2]]);
 
-    cameraMatrixLoc=gl.getUniformLocation( program, "cameraMatrix" );
-    cameraMatrix = lookAt(eye, at , cameraUp);
-    gl.uniformMatrix4fv(cameraMatrixLoc, false, flatten(cameraMatrix) );
+    let view = lookAt([pos[0],pos[1],pos[2]], [target[0],target[1],target[2]] , cameraUp);
+
+    gl.uniformMatrix4fv(gl.getUniformLocation( program, "cameraMatrix" ), false, flatten(view) );
 }
 
 function render(){
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    setCameraMatrix();
-    if(objectsReady) drawAllObjects();
-    else checkObjects();
-    requestAnimFrame(render);
+    if(objectsReady) {
+        draw();
+    }
+    else {
+        checkObjects();
+        requestAnimFrame(render);
+    }
 }
+
+function draw(){
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    if(moveCamera) {
+        cameraRotation+=1.0;
+        setCameraMatrix();
+    }
+
+    drawAllObjects();
+    requestAnimFrame(draw);
+}
+
 
 
 function checkObjects(){
-    let textured=true, obj=true, mtl=true;
-    objectArray.forEach(object=>{
-        textured=textured && object.textured;
-        obj=obj&& object.objParsed;
-        mtl=mtl&& object.mtlParsed;
-    })
-    objectsReady=obj && mtl;
+    for (let i = 0; i < objectArray.length; i++){
+        if(!objectArray[i].mtlParsed || !objectArray[i].objParsed){
+            objectsReady=false;
+            return;
+        }
+    }
+    objectsReady=true;
 }
 
 function drawAllObjects(){
-    objectArray.forEach(object=>{
-        object.faces.forEach(face=>{
-            materialAmbient=object.ambientMap.get(face.material);
-            materialDiffuse=object.diffuseMap.get(face.material);
-            materialSpecular=object.specularMap.get(face.material);
-            drawModel(object,face);
-        });
-    })
+    for (let i = 0; i < objectArray.length; i++){
+        for (let j = 0; j < objectArray[i].faces.length; j++){
+            drawFace(objectArray[i],objectArray[i].faces[j]);
+        }
+    }
 }
 
-function setTexture(texture){
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.bindTexture(gl.TEXTURE_2D,texture);
-}
+function drawFace(object,face){
+    configureTexture(object);
 
-function drawModel(object,face){
-    setTexture(object.texture);
-    //configureTexture(object);
-    //configureTexture(object);
     createBuffer(4,'vPosition',face.faceVertices);
     createBuffer(4,'vNormal',face.faceNormals);
     createBuffer(2,'vTexCoord',face.faceTexCoords);
@@ -162,8 +161,14 @@ function drawModel(object,face){
     modelMatrixLoc = gl.getUniformLocation( program, "modelMatrix" );
     modelMatrix = mult(rotateY(object.rotation),translate(object.position));
     gl.uniformMatrix4fv(modelMatrixLoc, false, flatten(modelMatrix) );
+
+    materialDiffuse=object.diffuseMap.get(face.material);
+    materialSpecular=object.specularMap.get(face.material);
+    gl.uniform4fv(gl.getUniformLocation(program, "shininess"), flatten(object.shiny));
+    lighting();
+
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
-    gl.drawArrays(gl.TRIANGLES,0,face.faceVertices.length);
+    gl.drawArrays(gl.TRIANGLE_FAN,0,face.faceVertices.length);
 }
 
 function configureTexture(object) {
@@ -187,29 +192,12 @@ function configureTexture(object) {
     gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
 }
 
-function staticLighting(){
-    gl.uniform4fv(gl.getUniformLocation(program, "lightPosition"), flatten(lightPosition));
-    gl.uniform1f(gl.getUniformLocation(program, "shininess"), materialShininess);
-    gl.uniform1i(gl.getUniformLocation(program, "lampOn"), lampOn);
-    lighting();
-}
-
 function lighting(){
-    let diffuseProduct = mult(lightDiffuse, materialDiffuse);
-    let specularProduct = mult(lightSpecular, materialSpecular);
-    let ambientProduct = mult(lightAmbient, materialAmbient);
-    gl.uniform4fv(gl.getUniformLocation(program, "diffuseProduct"), flatten(diffuseProduct));
-    gl.uniform4fv(gl.getUniformLocation(program, "specularProduct"), flatten(specularProduct));
-    gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct"), flatten(ambientProduct));
-}
-function createBuffer(size,attribute, array){
-    var cBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(array), gl.STATIC_DRAW );
-
-    var attributLoc =gl.getAttribLocation( program, attribute );
-    gl.vertexAttribPointer( attributLoc, size, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( attributLoc );
+    gl.uniform4fv(gl.getUniformLocation(program, "lightSpecular"), flatten(lightSpecular));
+    gl.uniform4fv(gl.getUniformLocation(program, "lightDiffuse"), flatten(lightDiffuse));
+    gl.uniform4fv(gl.getUniformLocation(program, "materialSpecular"), flatten(materialSpecular));
+    gl.uniform4fv(gl.getUniformLocation(program, "materialDiffuse"), flatten(materialDiffuse));
+    gl.uniform4fv(gl.getUniformLocation(program, "shininess"), flatten(materialShininess));
 }
 
 window.addEventListener("keypress",(event)=>{
@@ -222,7 +210,9 @@ function keypressInput(event){
         case 'l':
             /*Toggle light on and off. Make sure that ambient light is applied when off*/
             lampOn=!lampOn;
+            console.log(lampOn);
             gl.uniform1i(gl.getUniformLocation(program, "lampOn"), lampOn);
+
             break;
         case 'c':
             /*
@@ -259,18 +249,36 @@ function keypressInput(event){
 
 function loadObjects(){
     // Get the lamp
-    lamp = new Model("https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/lamp.obj", "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/lamp.mtl", vec3(0.0,0.0,0.0));
+    lamp = new Model(gl,"https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/lamp.obj", "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/lamp.mtl", vec3(0.0,0.0,0.0));
 
     // Get the car
-    car = new Model("https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/car.obj", "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/car.mtl",vec3(-3,0.0,0.0),-180);
+    car = new Model(gl,"https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/car.obj", "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/car.mtl",vec3(-3,0.0,0.0),-180);
 
     // Get the stop sign
-    stopSign = new Model("https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/stopsign.obj", "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/stopsign.mtl",vec3(-.850,0.0,-4.00),-90);
+    stopSign = new Model(gl,"https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/stopsign.obj", "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/stopsign.mtl",vec3(-.850,0.0,-4.00),-90);
 
     // Get the street
-    street = new Model("https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/stopsign.obj", "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/stopsign.mtl",vec3(.850,0.0,-4.00),90);
-    //street = new Model("https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/street.obj", "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/street.mtl",vec3(0.0,0.0,0.0));
+    street = new Model(gl,"https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/street.obj", "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/street.mtl",vec3(0.0,0.0,0.0));
 
     // Get the bunny
-    bunny = new Model("https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/bunny.obj", "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/bunny.mtl",vec3(0.0,0.0,0.0));
+    bunny = new Model(gl,"https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/bunny.obj", "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/bunny.mtl",vec3(0.0,0.0,0.0));
+}
+
+function createBuffer(size,attribute, array){
+    var buffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, buffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(array), gl.STATIC_DRAW );
+
+    var attributeLoc =gl.getAttribLocation( program, attribute );
+    gl.vertexAttribPointer( attributeLoc, size, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( attributeLoc );
+}
+
+function defaultLighting(){
+    gl.uniform4fv(gl.getUniformLocation(program, "lightAmbient"), flatten(lightAmbient));
+    gl.uniform4fv(gl.getUniformLocation(program, "materialAmbient"), flatten(materialAmbient));
+    gl.uniform4fv(gl.getUniformLocation(program, "lightPosition"), flatten(lightPosition));
+    gl.uniform1f(gl.getUniformLocation(program, "shininess"), materialShininess);
+    gl.uniform1i(gl.getUniformLocation(program, "lampOn"), lampOn);
+    lighting();
 }
